5edc4b688bbb6e86c52f18d526cb229e8d934156
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2024-07-05 23:52:49 +0300

    Добавил базовое приложение

--- library/settings.py
+++ library/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
     'catalog.apps.CatalogConfig',
     'blog.apps.BlogConfig',
     'personal.apps.PersonalConfig',
