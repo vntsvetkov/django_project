61d15c2e4652f8073d1b2db5bd2b2172e738c7b3
Николай Цветков <nv.tsvetko@yandex.ru>
2024-07-31 21:22:48 +0300

    Добавил модели

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=120, null=False)
+    text = models.TextField(null=False)
+    author = models.CharField(max_length=20)
+    publish_date = models.DateField(null=False, default=date.today())
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponseRedirect
+from .models import Post
 
 
 # Create your views here.
 def main(request: HttpRequest):
-    return render(request, 'posts.html')
+
+    # p = Post(title='Новость 2', text='Текст новости 2', author='Петров А.', publish_date='2024-07-31')
+    # p.save()
+
+    posts = Post.objects.values()
+    posts = posts.filter(publish_date='2024-07-31')
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'posts.html', context=context)
 
 
 def redirect(request: HttpRequest):
