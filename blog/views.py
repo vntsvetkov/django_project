6efdf513580041ae038f5232c9ad3a8bc753c19f
nv.tsvetkov <nv.tsvetkov@yandex.ru>
2024-08-01 22:05:20 +0300

    Реализовал страницу новостей

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponseRedirect
 from .models import Post
+from copy import deepcopy
 
 
 # Create your views here.
 def main(request: HttpRequest):
 
-    posts = Post.objects.values()
-    print(posts)
+    posts = Post.objects.all()
+    best_posts = deepcopy(posts)
     context = {
-        'posts': posts
+        'posts': posts,
+        'best_posts': best_posts,
     }
 
     return render(request, 'posts.html', context=context)
