5974300038ee4c6b7cc763a9707e9769abe5925b
Николай Цветков <nv.tsvetko@yandex.ru>
2024-08-02 20:59:55 +0300

    Добавил модели

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,17 @@ class HashTag(models.Model):
 
     name = models.CharField(max_length=120, null=False)
 
+    def __str__(self):
+        return self.name
+
+
 class Author(models.Model):
 
     name = models.CharField(max_length=120, null=False)
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
 class Post(models.Model):
@@ -15,9 +23,12 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images/', null=True)
     title = models.CharField(max_length=120, null=False)
     text = models.TextField(null=False)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        'Author',
+        on_delete=models.DO_NOTHING
+    )
     publish_date = models.DateField(null=False)
-
+    tags = models.ManyToManyField(HashTag)
     objects = models.Manager()
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,20 @@ def main(request: HttpRequest):
 
     posts = Post.objects.all()
     best_posts = deepcopy(posts)
+
+    # Запросить все теги связанные с постом
+
+    posts_id = Post.objects.values('id')
+
+    tags_dict = {}
+    for d in posts_id:
+        tags = Post.objects.get(id=d['id']).tags.all()
+        tags_dict[d['id']] = tags
+
     context = {
         'posts': posts,
         'best_posts': best_posts,
+        'tags': tags_dict,
     }
 
     return render(request, 'posts.html', context=context)
